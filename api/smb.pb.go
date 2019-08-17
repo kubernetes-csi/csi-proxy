540da89b9085447589e271d395c2bdfd0a80867e
Jean Rouge <rougej+github@gmail.com>
2019-08-17 12:09:01 -0700

    WIP: Review comments

--- api/smb.pb.go
+++ api/smb.pb.go
@@ -210,34 +210,173 @@ func (*MountSMBShareResponse) XXX_OneofWrappers() []interface{} {
 	}
 }
 
+type UnmountSMBShareRequest struct {
+	// Local host path at which the SMB share was previously staged.
+	//
+	// Restrictions:
+	// If given an absolute path, it needs to be under kubelet-csi-plugins-path,
+	// and specified with drive letter prefix: "X:\".
+	// If given a relative path, it will be understood as being relative to
+	// kubelet-csi-plugins-path.
+	HostPath             string   `protobuf:"bytes,1,opt,name=host_path,json=hostPath,proto3" json:"host_path,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *UnmountSMBShareRequest) Reset()         { *m = UnmountSMBShareRequest{} }
+func (m *UnmountSMBShareRequest) String() string { return proto.CompactTextString(m) }
+func (*UnmountSMBShareRequest) ProtoMessage()    {}
+func (*UnmountSMBShareRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_578488a9f93afe3f, []int{2}
+}
+
+func (m *UnmountSMBShareRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_UnmountSMBShareRequest.Unmarshal(m, b)
+}
+func (m *UnmountSMBShareRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_UnmountSMBShareRequest.Marshal(b, m, deterministic)
+}
+func (m *UnmountSMBShareRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_UnmountSMBShareRequest.Merge(m, src)
+}
+func (m *UnmountSMBShareRequest) XXX_Size() int {
+	return xxx_messageInfo_UnmountSMBShareRequest.Size(m)
+}
+func (m *UnmountSMBShareRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_UnmountSMBShareRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_UnmountSMBShareRequest proto.InternalMessageInfo
+
+func (m *UnmountSMBShareRequest) GetHostPath() string {
+	if m != nil {
+		return m.HostPath
+	}
+	return ""
+}
+
+type UnmountSMBShareResponse struct {
+	Success bool `protobuf:"varint,1,opt,name=success,proto3" json:"success,omitempty"`
+	// Types that are valid to be assigned to Error:
+	//	*UnmountSMBShareResponse_CmdletError
+	//	*UnmountSMBShareResponse_ErrorMessage
+	Error                isUnmountSMBShareResponse_Error `protobuf_oneof:"error"`
+	XXX_NoUnkeyedLiteral struct{}                        `json:"-"`
+	XXX_unrecognized     []byte                          `json:"-"`
+	XXX_sizecache        int32                           `json:"-"`
+}
+
+func (m *UnmountSMBShareResponse) Reset()         { *m = UnmountSMBShareResponse{} }
+func (m *UnmountSMBShareResponse) String() string { return proto.CompactTextString(m) }
+func (*UnmountSMBShareResponse) ProtoMessage()    {}
+func (*UnmountSMBShareResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_578488a9f93afe3f, []int{3}
+}
+
+func (m *UnmountSMBShareResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_UnmountSMBShareResponse.Unmarshal(m, b)
+}
+func (m *UnmountSMBShareResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_UnmountSMBShareResponse.Marshal(b, m, deterministic)
+}
+func (m *UnmountSMBShareResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_UnmountSMBShareResponse.Merge(m, src)
+}
+func (m *UnmountSMBShareResponse) XXX_Size() int {
+	return xxx_messageInfo_UnmountSMBShareResponse.Size(m)
+}
+func (m *UnmountSMBShareResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_UnmountSMBShareResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_UnmountSMBShareResponse proto.InternalMessageInfo
+
+func (m *UnmountSMBShareResponse) GetSuccess() bool {
+	if m != nil {
+		return m.Success
+	}
+	return false
+}
+
+type isUnmountSMBShareResponse_Error interface {
+	isUnmountSMBShareResponse_Error()
+}
+
+type UnmountSMBShareResponse_CmdletError struct {
+	CmdletError *CmdletError `protobuf:"bytes,2,opt,name=cmdlet_error,json=cmdletError,proto3,oneof"`
+}
+
+type UnmountSMBShareResponse_ErrorMessage struct {
+	ErrorMessage string `protobuf:"bytes,3,opt,name=error_message,json=errorMessage,proto3,oneof"`
+}
+
+func (*UnmountSMBShareResponse_CmdletError) isUnmountSMBShareResponse_Error() {}
+
+func (*UnmountSMBShareResponse_ErrorMessage) isUnmountSMBShareResponse_Error() {}
+
+func (m *UnmountSMBShareResponse) GetError() isUnmountSMBShareResponse_Error {
+	if m != nil {
+		return m.Error
+	}
+	return nil
+}
+
+func (m *UnmountSMBShareResponse) GetCmdletError() *CmdletError {
+	if x, ok := m.GetError().(*UnmountSMBShareResponse_CmdletError); ok {
+		return x.CmdletError
+	}
+	return nil
+}
+
+func (m *UnmountSMBShareResponse) GetErrorMessage() string {
+	if x, ok := m.GetError().(*UnmountSMBShareResponse_ErrorMessage); ok {
+		return x.ErrorMessage
+	}
+	return ""
+}
+
+// XXX_OneofWrappers is for the internal use of the proto package.
+func (*UnmountSMBShareResponse) XXX_OneofWrappers() []interface{} {
+	return []interface{}{
+		(*UnmountSMBShareResponse_CmdletError)(nil),
+		(*UnmountSMBShareResponse_ErrorMessage)(nil),
+	}
+}
+
 func init() {
 	proto.RegisterType((*MountSMBShareRequest)(nil), "api.MountSMBShareRequest")
 	proto.RegisterType((*MountSMBShareResponse)(nil), "api.MountSMBShareResponse")
+	proto.RegisterType((*UnmountSMBShareRequest)(nil), "api.UnmountSMBShareRequest")
+	proto.RegisterType((*UnmountSMBShareResponse)(nil), "api.UnmountSMBShareResponse")
 }
 
 func init() { proto.RegisterFile("smb.proto", fileDescriptor_578488a9f93afe3f) }
 
 var fileDescriptor_578488a9f93afe3f = []byte{
-	// 304 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x91, 0xd1, 0x4a, 0xf3, 0x30,
-	0x14, 0xc7, 0xd7, 0x6f, 0xdf, 0x5c, 0x7b, 0xb6, 0x81, 0x04, 0x85, 0x58, 0x6f, 0xe6, 0x40, 0xd8,
-	0xd5, 0x2e, 0x26, 0xbe, 0xc0, 0x86, 0x30, 0x2f, 0x06, 0x23, 0xbd, 0xb7, 0x64, 0xdd, 0xc1, 0x0e,
-	0x96, 0x26, 0xe6, 0xa4, 0xea, 0xde, 0xc4, 0x67, 0xf0, 0x29, 0xa5, 0x09, 0x55, 0x94, 0x5d, 0xfe,
-	0x7f, 0xbf, 0x53, 0xfa, 0x3f, 0x27, 0x90, 0x90, 0xda, 0xce, 0x8c, 0xd5, 0x4e, 0xb3, 0xae, 0x34,
-	0xfb, 0x74, 0x88, 0xd6, 0x6a, 0x4b, 0x01, 0x4d, 0x3e, 0x23, 0xb8, 0x58, 0xeb, 0xba, 0x72, 0xd9,
-	0x7a, 0x91, 0x95, 0xd2, 0xa2, 0xc0, 0x97, 0x1a, 0xc9, 0xb1, 0x1b, 0x18, 0x5a, 0x54, 0xda, 0x61,
-	0x4e, 0x0d, 0xe6, 0xd1, 0x38, 0x9a, 0x26, 0x62, 0x10, 0x98, 0x9f, 0x64, 0xd7, 0x90, 0x94, 0x9a,
-	0x5c, 0x6e, 0xa4, 0x2b, 0xf9, 0x3f, 0xef, 0xe3, 0x06, 0x6c, 0xa4, 0x2b, 0x59, 0x0a, 0xb1, 0x45,
-	0xb9, 0xd3, 0xd5, 0xe1, 0xc8, 0xbb, 0xe3, 0x68, 0x1a, 0x8b, 0xef, 0xdc, 0xb8, 0x9a, 0xd0, 0x56,
-	0x52, 0x21, 0xff, 0x1f, 0xbe, 0x6b, 0x73, 0xe3, 0x8c, 0x24, 0x7a, 0xd3, 0x76, 0xc7, 0x7b, 0xc1,
-	0xb5, 0x79, 0xf2, 0x11, 0xc1, 0xe5, 0x9f, 0xb2, 0x64, 0x74, 0x45, 0xc8, 0x38, 0xf4, 0xa9, 0x2e,
-	0x0a, 0x24, 0xf2, 0x45, 0x63, 0xd1, 0x46, 0x76, 0x0f, 0xc3, 0x42, 0xed, 0x0e, 0xe8, 0x72, 0xbf,
-	0xb7, 0xef, 0x39, 0x98, 0x9f, 0xcf, 0xa4, 0xd9, 0xcf, 0x96, 0x5e, 0x3c, 0x34, 0x7c, 0xd5, 0x11,
-	0x83, 0xe2, 0x27, 0xb2, 0x5b, 0x18, 0xf9, 0xf9, 0x5c, 0x21, 0x91, 0x7c, 0x46, 0xbf, 0x43, 0xb2,
-	0xea, 0x88, 0x70, 0xbe, 0x75, 0xa0, 0x8b, 0x3e, 0xf4, 0x7c, 0x9e, 0x3f, 0x01, 0xcb, 0xd4, 0x76,
-	0x99, 0x3d, 0x6e, 0xac, 0x7e, 0x3f, 0x66, 0x68, 0x5f, 0xf7, 0x05, 0xb2, 0x15, 0x8c, 0x7e, 0xf5,
-	0x65, 0x57, 0xfe, 0xbf, 0xa7, 0x0e, 0x9e, 0xa6, 0xa7, 0x54, 0x58, 0x6f, 0xd2, 0xd9, 0x9e, 0xf9,
-	0xe7, 0xba, 0xfb, 0x0a, 0x00, 0x00, 0xff, 0xff, 0x74, 0x87, 0x05, 0x83, 0xce, 0x01, 0x00, 0x00,
+	// 352 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xcc, 0x92, 0xcf, 0x4e, 0xea, 0x40,
+	0x14, 0xc6, 0x99, 0xcb, 0xe5, 0xd2, 0x1e, 0x20, 0xd7, 0x4c, 0xfc, 0x53, 0x8b, 0x0b, 0x6c, 0x62,
+	0xc2, 0x8a, 0x05, 0x86, 0x17, 0x80, 0x98, 0xe0, 0x02, 0x43, 0xda, 0xb8, 0x6e, 0x86, 0x72, 0x62,
+	0x49, 0x68, 0xa7, 0xce, 0x99, 0xaa, 0xbc, 0x89, 0xf1, 0x11, 0x5c, 0xf8, 0x8c, 0xa6, 0xd3, 0x54,
+	0x03, 0xf6, 0x01, 0x5c, 0x7e, 0xdf, 0x6f, 0x4e, 0x7b, 0xbe, 0x6f, 0x06, 0x6c, 0x4a, 0x56, 0xa3,
+	0x4c, 0x49, 0x2d, 0x79, 0x53, 0x64, 0x1b, 0xb7, 0x8b, 0x4a, 0x49, 0x45, 0xa5, 0xe5, 0xbd, 0x33,
+	0x38, 0x5e, 0xc8, 0x3c, 0xd5, 0xc1, 0x62, 0x1a, 0xc4, 0x42, 0xa1, 0x8f, 0x8f, 0x39, 0x92, 0xe6,
+	0x97, 0xd0, 0x55, 0x98, 0x48, 0x8d, 0x21, 0x15, 0xb6, 0xc3, 0x06, 0x6c, 0x68, 0xfb, 0x9d, 0xd2,
+	0x33, 0x27, 0x79, 0x1f, 0xec, 0x58, 0x92, 0x0e, 0x33, 0xa1, 0x63, 0xe7, 0x8f, 0xe1, 0x56, 0x61,
+	0x2c, 0x85, 0x8e, 0xb9, 0x0b, 0x96, 0x42, 0xb1, 0x96, 0xe9, 0x76, 0xe7, 0x34, 0x07, 0x6c, 0x68,
+	0xf9, 0x5f, 0xba, 0x60, 0x39, 0xa1, 0x4a, 0x45, 0x82, 0xce, 0xdf, 0x72, 0xae, 0xd2, 0x05, 0xcb,
+	0x04, 0xd1, 0xb3, 0x54, 0x6b, 0xa7, 0x55, 0xb2, 0x4a, 0x7b, 0xaf, 0x0c, 0x4e, 0x0e, 0x96, 0xa5,
+	0x4c, 0xa6, 0x84, 0xdc, 0x81, 0x36, 0xe5, 0x51, 0x84, 0x44, 0x66, 0x51, 0xcb, 0xaf, 0x24, 0x9f,
+	0x40, 0x37, 0x4a, 0xd6, 0x5b, 0xd4, 0xa1, 0xc9, 0x6d, 0xf6, 0xec, 0x8c, 0x8f, 0x46, 0x22, 0xdb,
+	0x8c, 0x66, 0x06, 0xdc, 0x14, 0xfe, 0xbc, 0xe1, 0x77, 0xa2, 0x6f, 0xc9, 0xaf, 0xa0, 0x67, 0xce,
+	0x87, 0x09, 0x12, 0x89, 0x07, 0x34, 0x19, 0xec, 0x79, 0xc3, 0x2f, 0xeb, 0x5b, 0x94, 0xee, 0xb4,
+	0x0d, 0x2d, 0xa3, 0xbd, 0x09, 0x9c, 0xde, 0xa7, 0x49, 0x5d, 0x91, 0x7b, 0x2d, 0xb1, 0xfd, 0x96,
+	0xbc, 0x37, 0x06, 0x67, 0x3f, 0xe6, 0x7e, 0x49, 0xa6, 0xf1, 0x07, 0x03, 0x1e, 0x24, 0xab, 0x59,
+	0x70, 0xbb, 0x54, 0xf2, 0x65, 0x17, 0xa0, 0x7a, 0xda, 0x44, 0xc8, 0xe7, 0xd0, 0xdb, 0xbb, 0x04,
+	0x7e, 0x6e, 0x7e, 0x5c, 0xf7, 0x8a, 0x5c, 0xb7, 0x0e, 0x95, 0xf9, 0xbc, 0x06, 0xbf, 0x83, 0xff,
+	0x07, 0xe1, 0x79, 0xdf, 0x0c, 0xd4, 0x57, 0xe9, 0x5e, 0xd4, 0xc3, 0xea, 0x7b, 0xab, 0x7f, 0xe6,
+	0x4d, 0x5f, 0x7f, 0x06, 0x00, 0x00, 0xff, 0xff, 0x91, 0x75, 0x02, 0x1d, 0xf3, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -254,6 +393,8 @@ const _ = grpc.SupportPackageIsVersion4
 type SmbCSIProxyServiceClient interface {
 	// MountSMBShare mounts a remote share over SMB on the host at the requested global staging path.
 	MountSMBShare(ctx context.Context, in *MountSMBShareRequest, opts ...grpc.CallOption) (*MountSMBShareResponse, error)
+	// UnmountSMBShare unmounts a SMB remote share that was previously mounted with MountSMBShare.
+	UnmountSMBShare(ctx context.Context, in *UnmountSMBShareRequest, opts ...grpc.CallOption) (*UnmountSMBShareResponse, error)
 }
 
 type smbCSIProxyServiceClient struct {
@@ -273,10 +414,21 @@ func (c *smbCSIProxyServiceClient) MountSMBShare(ctx context.Context, in *MountS
 	return out, nil
 }
 
+func (c *smbCSIProxyServiceClient) UnmountSMBShare(ctx context.Context, in *UnmountSMBShareRequest, opts ...grpc.CallOption) (*UnmountSMBShareResponse, error) {
+	out := new(UnmountSMBShareResponse)
+	err := c.cc.Invoke(ctx, "/api.SmbCSIProxyService/UnmountSMBShare", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // SmbCSIProxyServiceServer is the server API for SmbCSIProxyService service.
 type SmbCSIProxyServiceServer interface {
 	// MountSMBShare mounts a remote share over SMB on the host at the requested global staging path.
 	MountSMBShare(context.Context, *MountSMBShareRequest) (*MountSMBShareResponse, error)
+	// UnmountSMBShare unmounts a SMB remote share that was previously mounted with MountSMBShare.
+	UnmountSMBShare(context.Context, *UnmountSMBShareRequest) (*UnmountSMBShareResponse, error)
 }
 
 // UnimplementedSmbCSIProxyServiceServer can be embedded to have forward compatible implementations.
@@ -285,6 +437,9 @@ type UnimplementedSmbCSIProxyServiceServer struct {
 
 func (*UnimplementedSmbCSIProxyServiceServer) MountSMBShare(ctx context.Context, req *MountSMBShareRequest) (*MountSMBShareResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method MountSMBShare not implemented")
+}
+func (*UnimplementedSmbCSIProxyServiceServer) UnmountSMBShare(ctx context.Context, req *UnmountSMBShareRequest) (*UnmountSMBShareResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UnmountSMBShare not implemented")
 }
 
 func RegisterSmbCSIProxyServiceServer(s *grpc.Server, srv SmbCSIProxyServiceServer) {
@@ -309,6 +464,24 @@ func _SmbCSIProxyService_MountSMBShare_Handler(srv interface{}, ctx context.Cont
 	return interceptor(ctx, in, info, handler)
 }
 
+func _SmbCSIProxyService_UnmountSMBShare_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UnmountSMBShareRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SmbCSIProxyServiceServer).UnmountSMBShare(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/api.SmbCSIProxyService/UnmountSMBShare",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SmbCSIProxyServiceServer).UnmountSMBShare(ctx, req.(*UnmountSMBShareRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _SmbCSIProxyService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "api.SmbCSIProxyService",
 	HandlerType: (*SmbCSIProxyServiceServer)(nil),
@@ -316,6 +489,10 @@ var _SmbCSIProxyService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "MountSMBShare",
 			Handler:    _SmbCSIProxyService_MountSMBShare_Handler,
+		},
+		{
+			MethodName: "UnmountSMBShare",
+			Handler:    _SmbCSIProxyService_UnmountSMBShare_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
