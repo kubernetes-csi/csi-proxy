c83b392f4554a9e6283e8662b0f504c83bf880e5
Deep Debroy <ddebroy@docker.com>
2020-07-20 01:50:17 -0700

    Run csiproxy as a Windows Service
    Signed-off-by: Deep Debroy <ddebroy@docker.com>

--- cmd/csi-proxy/main.go
+++ cmd/csi-proxy/main.go
@@ -13,19 +13,35 @@ import (
 	smbsrv "github.com/kubernetes-csi/csi-proxy/internal/server/smb"
 	srvtypes "github.com/kubernetes-csi/csi-proxy/internal/server/types"
 	volumesrv "github.com/kubernetes-csi/csi-proxy/internal/server/volume"
+	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/svc"
 	"k8s.io/klog"
 )
 
 var (
 	kubeletCSIPluginsPath = flag.String("kubelet-csi-plugins-path", `C:\var\lib\kubelet`, "Prefix path of the Kubelet plugin directory in the host file system")
 	kubeletPodPath        = flag.String("kubelet-pod-path", `C:\var\lib\kubelet`, "Prefix path of the kubelet pod directory in the host file system")
+	windowsSvc            = flag.Bool("windows-service", false, "Configure as a Windows Service")
+	service               *handler
 )
+
+type handler struct {
+	tosvc   chan bool
+	fromsvc chan error
+}
 
 func main() {
 	defer klog.Flush()
 	klog.InitFlags(nil)
 
 	flag.Parse()
+
+	if *windowsSvc {
+		if err := initService(); err != nil {
+			panic(err)
+		}
+	}
+
 	apiGroups, err := apiGroups()
 	if err != nil {
 		panic(err)
@@ -67,4 +83,57 @@ func apiGroups() ([]srvtypes.APIGroup, error) {
 		volumesrv,
 		smbsrv,
 	}, nil
+}
+
+// configure as a Windows service managed by Windows SCM
+// code borrowed from
+// https://github.com/kubernetes/kubernetes/blob/323f34858de18b862d43c40b2cced65ad8e24052/pkg/windows/service/service.go
+func initService() error {
+	h := &handler{
+		tosvc:   make(chan bool),
+		fromsvc: make(chan error),
+	}
+
+	service = h
+	var err error
+	go func() {
+		err = svc.Run("csiproxy", h)
+		h.fromsvc <- err
+	}()
+
+	// Wait for the first signal from the service handler.
+	err = <-h.fromsvc
+	if err != nil {
+		return err
+	}
+	klog.Infof("Running as a Windows service.")
+	return nil
+}
+
+func (h *handler) Execute(_ []string, r <-chan svc.ChangeRequest, s chan<- svc.Status) (bool, uint32) {
+	s <- svc.Status{State: svc.StartPending, Accepts: 0}
+	// Unblock initService()
+	h.fromsvc <- nil
+
+	s <- svc.Status{State: svc.Running, Accepts: svc.AcceptStop | svc.AcceptShutdown | svc.Accepted(windows.SERVICE_ACCEPT_PARAMCHANGE)}
+	klog.Infof("Windows Service initialized through SCM")
+Loop:
+	for {
+		select {
+		case <-h.tosvc:
+			break Loop
+		case c := <-r:
+			switch c.Cmd {
+			case svc.Interrogate:
+				s <- c.CurrentStatus
+			case svc.Stop, svc.Shutdown:
+				// todo: need to add a ctx to servers
+				// from main and cancel it from here
+				s <- svc.Status{State: svc.StopPending}
+				break Loop
+			}
+		}
+	}
+
+	return false, 0
 }
--- go.mod
+++ go.mod
@@ -22,7 +22,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
-	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
+	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20200205142000-a86caf926a67 // indirect
 	google.golang.org/grpc v1.27.1
