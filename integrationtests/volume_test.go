f063c0a997bf26842120ee6afa6dc430551af03b
Mauricio Poppe <mauriciopoppe@google.com>
2021-05-24 20:02:55 +0000

    Resize volume to 1.5x instead of 2x

--- integrationtests/volume_test.go
+++ integrationtests/volume_test.go
@@ -330,9 +330,9 @@ func simpleE2e(t *testing.T) {
 		t.Fatalf("volumeStatsResponse.TotalBytes reported is not valid, it is %v", volumeStatsResponse.TotalBytes)
 	}
 
-	// Resize from 1G to 2G
+	// Resize the disk to twice its size (from 1GB to 2GB)
 	oldSize := volumeStatsResponse.TotalBytes
-	newSize := int64(2 * 1024 * 1024 * 1024)
+	newSize := int64(oldSize * 2)
 
 	// To resize a volume we need to resize the virtual hard disk first and then the partition
 	cmd := fmt.Sprintf("Resize-VHD -Path %s -SizeBytes %d", vhdxPath, newSize)
@@ -340,9 +340,17 @@ func simpleE2e(t *testing.T) {
 		t.Fatalf("Error: %v. Command: %q. Out: %s.", err, cmd, out)
 	}
 
+	// This is the max size a partition may be resized to (less than 2GB)
+	//
+	//    Get-PartitionSupportedSize -DiskNumber 7 -PartitionNumber 2 | ConvertTo-Json
+	//    {
+	//    	"SizeMin":  404725760,
+	//    	"SizeMax":  2130689536
+	//    }
 	resizeVolumeRequest := &v1beta3.ResizeVolumeRequest{
-		VolumeId:  volumeID,
-		SizeBytes: newSize,
+		VolumeId: volumeID,
+		// resize the partition to 1.5x times instead
+		SizeBytes: int64(float32(oldSize) * 1.5),
 	}
 
 	t.Logf("Attempt to resize volume from sizeBytes=%d to sizeBytes=%d", oldSize, newSize)
@@ -357,8 +365,8 @@ func simpleE2e(t *testing.T) {
 		t.Fatalf("VolumeStats request after resize error: %v", err)
 	}
 
-	if volumeStatsResponse.TotalBytes >= oldSize {
-		t.Fatalf("VolumeSize reported is not smaller after resize, it is %v", volumeStatsResponse.TotalBytes)
+	if volumeStatsResponse.TotalBytes <= oldSize {
+		t.Fatalf("VolumeSize reported should be greater than the old size, it is %v", volumeStatsResponse.TotalBytes)
 	}
 
 	volumeDiskNumberRequest := &v1beta3.GetDiskNumberFromVolumeIDRequest{
