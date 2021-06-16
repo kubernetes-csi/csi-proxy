437487ce237f6526a65286130171a8e60407aa51
Mauricio Poppe <mauriciopoppe@google.com>
2021-06-16 05:07:21 +0000

    Bump filesystem v1beta2 API to v1

--- pkg/server/filesystem/api_group_generated.go
+++ pkg/server/filesystem/api_group_generated.go
@@ -5,6 +5,7 @@ package filesystem
 import (
 	"github.com/kubernetes-csi/csi-proxy/client/apiversion"
 	"github.com/kubernetes-csi/csi-proxy/pkg/server/filesystem/impl"
+	v1 "github.com/kubernetes-csi/csi-proxy/pkg/server/filesystem/impl/v1"
 	"github.com/kubernetes-csi/csi-proxy/pkg/server/filesystem/impl/v1alpha1"
 	"github.com/kubernetes-csi/csi-proxy/pkg/server/filesystem/impl/v1beta1"
 	"github.com/kubernetes-csi/csi-proxy/pkg/server/filesystem/impl/v1beta2"
@@ -20,6 +21,7 @@ func (s *Server) VersionedAPIs() []*srvtypes.VersionedAPI {
 	v1alpha1Server := v1alpha1.NewVersionedServer(s)
 	v1beta1Server := v1beta1.NewVersionedServer(s)
 	v1beta2Server := v1beta2.NewVersionedServer(s)
+	v1Server := v1.NewVersionedServer(s)
 
 	return []*srvtypes.VersionedAPI{
 		{
@@ -36,6 +38,11 @@ func (s *Server) VersionedAPIs() []*srvtypes.VersionedAPI {
 			Group:      name,
 			Version:    apiversion.NewVersionOrPanic("v1beta2"),
 			Registrant: v1beta2Server.Register,
+		},
+		{
+			Group:      name,
+			Version:    apiversion.NewVersionOrPanic("v1"),
+			Registrant: v1Server.Register,
 		},
 	}
 }
