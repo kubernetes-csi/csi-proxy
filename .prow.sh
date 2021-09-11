c7bd371834d781042d31df64e46b7696ecbbe2e3
Mauricio Poppe <mauriciopoppe@google.com>
2021-09-11 00:12:21 +0000

    Call ensure_paths in .prow.sh
    There's a mismatch between the go version installed in the prow job
    (1.17) and the version wanted to do the build (1.16), because of it
    there's a new path in run_with_go that will attempt to download 1.16 but
    fail because an env var is not set.
    
    Calling ensure_paths makes sure that the env_var is set, this is a
    change outside of release-tools.

--- .prow.sh
+++ .prow.sh
@@ -5,9 +5,12 @@
 # # Only these tests make sense for csi-proxy
 : ${CSI_PROW_TESTS:="unit"}
 : ${CSI_PROW_BUILD_PLATFORMS:="windows amd64 .exe"}
- 
+
 . release-tools/prow.sh
 
-#main
+# This creates the CSI_PROW_WORK directory that is needed by run_with_go.
+ensure_paths
+
+# main
 run_with_go "${CSI_PROW_GO_VERSION_BUILD}" make all "GOFLAGS_VENDOR=${GOFLAGS_VENDOR}" "BUILD_PLATFORMS=${CSI_PROW_BUILD_PLATFORMS}"
 run_with_go "${CSI_PROW_GO_VERSION_BUILD}" make -k test "GOFLAGS_VENDOR=${GOFLAGS_VENDOR}" 2>&1 | make_test_to_junit
