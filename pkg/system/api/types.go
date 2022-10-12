158866f436cd190943e913e2f9eb426b83b7269d
Alexander Ding <ajding@google.com>
2022-10-12 21:54:39 +0000

    feat: migrate system API group to library model

---
+++ pkg/system/api/types.go
@@ -0,0 +1,12 @@
+package api
+
+type ServiceInfo struct {
+	// Service display name
+	DisplayName string `json:"DisplayName"`
+
+	// Service start type
+	StartType uint32 `json:"StartType"`
+
+	// Service status
+	Status uint32 `json:"Status"`
+}
---
+++ pkg/system/types.go
@@ -0,0 +1,70 @@
+package system
+
+type GetBIOSSerialNumberRequest struct {
+}
+
+type GetBIOSSerialNumberResponse struct {
+	SerialNumber string
+}
+
+type StartServiceRequest struct {
+	// Service name (as listed in System\CCS\Services keys)
+	Name string
+}
+
+type StartServiceResponse struct {
+	// Intentionally empty
+}
+
+type StopServiceRequest struct {
+	// Service name (as listed in System\CCS\Services keys)
+	Name string
+
+	// Forces stopping of services that has dependent services
+	Force bool
+}
+
+type StopServiceResponse struct {
+	// Intentionally empty
+}
+
+type ServiceStatus uint32
+
+const (
+	SERVICE_STATUS_UNKNOWN ServiceStatus = iota
+	SERVICE_STATUS_STOPPED
+	SERVICE_STATUS_START_PENDING
+	SERVICE_STATUS_STOP_PENDING
+	SERVICE_STATUS_RUNNING
+	SERVICE_STATUS_CONTINUE_PENDING
+	SERVICE_STATUS_PAUSE_PENDING
+	SERVICE_STATUS_PAUSED
+)
+
+type Startype uint32
+
+const (
+	START_TYPE_BOOT Startype = iota
+	START_TYPE_SYSTEM
+	START_TYPE_AUTOMATIC
+	START_TYPE_MANUAL
+	START_TYPE_DISABLED
+)
+
+type GetServiceRequest struct {
+	// Service name (as listed in System\CCS\Services keys)
+	Name string
+}
+
+type GetServiceResponse struct {
+	// Service display name
+	DisplayName string
+
+	// Service start type.
+	// Used to control whether a service will start on boot, and if so on which
+	// boot phase.
+	StartType Startype
+
+	// Service status, e.g. stopped, running, paused
+	Status ServiceStatus
+}
