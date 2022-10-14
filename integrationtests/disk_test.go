f7920f50d18366c2f891e63115019272aa5f45cd
Alexander Ding <ajding@google.com>
2022-10-14 17:50:23 +0000

    test: remove v1 tests

--- integrationtests/disk_test.go
+++ integrationtests/disk_test.go
@@ -191,24 +191,3 @@ func TestDisk(t *testing.T) {
 		require.NoError(t, err)
 	})
 }
-
-// This test is meant to run on GCE where the page83 ID of the first disk contains
-// the host name
-// Skip on Github Actions as it is expected to fail
-func TestDiskAPIGroup(t *testing.T) {
-	t.Run("v1Tests", func(t *testing.T) {
-		v1DiskTests(t)
-	})
-	t.Run("v1beta3Tests", func(t *testing.T) {
-		v1beta3DiskTests(t)
-	})
-	t.Run("v1beta2Tests", func(t *testing.T) {
-		v1beta2DiskTests(t)
-	})
-	t.Run("v1beta1Tests", func(t *testing.T) {
-		v1beta1DiskTests(t)
-	})
-	t.Run("v1alpha1Tests", func(t *testing.T) {
-		v1alpha1DiskTests(t)
-	})
-}
