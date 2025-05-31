018326726fe4ad82853f8aca2867b26380cc4b75
Zhongcheng Lao <zhongcheng.lao@broadcom.com>
2025-05-31 12:55:22 +0800

    Use associator to find iSCSI WMI instances

--- pkg/os/iscsi/api.go
+++ pkg/os/iscsi/api.go
@@ -60,7 +60,7 @@ func (APIImplementor) DiscoverTargetPortal(portal *TargetPortal) ([]string, erro
 		return nil, err
 	}
 
-	targets, err := cim.ListISCSITargetsByTargetPortalWithFilters(nil, []*storage.MSFT_iSCSITargetPortal{instance})
+	targets, err := cim.ListISCSITargetsByTargetPortal([]*storage.MSFT_iSCSITargetPortal{instance})
 	if err != nil {
 		return nil, err
 	}
@@ -125,7 +125,7 @@ func (APIImplementor) RemoveTargetPortal(portal *TargetPortal) error {
 }
 
 func (APIImplementor) ConnectTarget(portal *TargetPortal, iqn string, authType string, chapUser string, chapSecret string) error {
-	target, err := cim.QueryISCSITarget(portal.Address, portal.Port, iqn, nil)
+	target, err := cim.QueryISCSITarget(portal.Address, portal.Port, iqn)
 	if err != nil {
 		return err
 	}
@@ -151,7 +151,7 @@ func (APIImplementor) ConnectTarget(portal *TargetPortal, iqn string, authType s
 }
 
 func (APIImplementor) DisconnectTarget(portal *TargetPortal, iqn string) error {
-	target, err := cim.QueryISCSITarget(portal.Address, portal.Port, iqn, nil)
+	target, err := cim.QueryISCSITarget(portal.Address, portal.Port, iqn)
 	if err != nil {
 		return err
 	}
@@ -167,7 +167,7 @@ func (APIImplementor) DisconnectTarget(portal *TargetPortal, iqn string) error {
 	}
 
 	// get session
-	session, err := cim.QueryISCSISessionByTarget(target, nil)
+	session, err := cim.QueryISCSISessionByTarget(target)
 	if err != nil {
 		return fmt.Errorf("error query session of  target %s from target portal at (%s:%d). err: %w", iqn, portal.Address, portal.Port, err)
 	}
@@ -198,9 +198,7 @@ func (APIImplementor) DisconnectTarget(portal *TargetPortal, iqn string) error {
 }
 
 func (APIImplementor) GetTargetDisks(portal *TargetPortal, iqn string) ([]string, error) {
-	// Converting DiskNumber to string for compatibility with disk api group
-	// Not using pipeline in order to validate that items are non-empty
-	target, err := cim.QueryISCSITarget(portal.Address, portal.Port, iqn, nil)
+	target, err := cim.QueryISCSITarget(portal.Address, portal.Port, iqn)
 	if err != nil {
 		return nil, err
 	}
@@ -215,8 +213,7 @@ func (APIImplementor) GetTargetDisks(portal *TargetPortal, iqn string) ([]string
 		return nil, nil
 	}
 
-	disks, err := cim.ListDisksByTarget(target, []string{})
-
+	disks, err := cim.ListDisksByTarget(target)
 	if err != nil {
 		return nil, fmt.Errorf("error getting target disks on target %s from target portal at (%s:%d). err: %w", iqn, portal.Address, portal.Port, err)
 	}
